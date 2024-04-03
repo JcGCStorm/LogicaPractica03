22b211bb19d1770fa21daf798dbf3d26285b112e
JcGCStorm <111623783+JcGCStorm@users.noreply.github.com>
2024-04-03 15:28:38 -0600

    Update FormasNormales.hs, update FNN
    Saacatunga

--- Src/FormasNormales.hs
+++ Src/FormasNormales.hs
@@ -6,7 +6,13 @@ import Operadores
 -- Función que dada una fórmula f, regresa una fórmula equivalente a f
 -- en su Forma Normal Negativa.
 fnn :: Prop -> Prop
-fnn = error "D:"
+fnn :: Prop -> Prop
+fnn (Var p) = Var p
+fnn (Neg (Neg p)) = fnn p
+fnn (Neg (Conj p q)) = Disy (fnn (Neg p)) (fnn (Neg q))
+fnn (Neg (Disy p q)) = Conj (fnn (Neg p)) (fnn (Neg q))
+fnn (Conj p q) = Conj (fnn p) (fnn q)
+fnn (Disy p q) = Disy (fnn p) (fnn q)
 
 -- Función que dada una fórmula f, regresa una fórmula equivalente a f
 -- en su Forma Normal Conjuntiva.
@@ -20,6 +26,12 @@ fnd = error "D:"
 
 -- ejemplo 1 para forma normal negativa
 fnn1 = error "D:"
+-- Ejemplo: ¬(p ∨ q)
+formulaEjemplo :: Prop
+formulaEjemplo = Neg (Disy (Var "p") (Var "q"))
+
+formaNormalNegativa :: Prop
+formaNormalNegativa = fnn formulaEjemplo
 
 -- ejemplo 2 para forma normal negativa
 fnn2 = error "D:"
